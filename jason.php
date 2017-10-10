a04e0c4257be2fcddedfa6ca591c24153d3dd0fa
kevinjmk <32671315+kevinjmk@users.noreply.github.com>
2017-10-10 13:08:23 +0100

    Create jason.php

---
+++ jason.php
@@ -0,0 +1,7 @@
+<?php
+$items = array();
+for($i=0; $i < 10; $i++) { $items[$i] = "this is message to andi $i";
+}
+header('content-Type: application/jason');
+$jsonOutput = jason_encode($items);
+echo $jsonOutput;
