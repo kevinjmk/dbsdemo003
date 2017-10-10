8f446e0d981d700f2352adea32b7e8050864ae59
kevinjmk <32671315+kevinjmk@users.noreply.github.com>
2017-10-10 13:21:53 +0100

    Update and rename jason.php to json.php

--- json.php
+++ json.php
@@ -4,5 +4,5 @@ for($i=0; $i < 10; $i++) {
   $items[$i]="this is message to andi $i";
 }
 header('content-Type: application/jason');
-$jsonOutput=jason_encode($items);
+$jsonOutput=json_encode($items);
 echo $jsonOutput;
