01ab8953a774245e1ef092f4ecd37dfc810169bd
kevinjmk <32671315+kevinjmk@users.noreply.github.com>
2017-10-10 13:11:55 +0100

    Update jason.php

--- jason.php
+++ jason.php
@@ -1,7 +1,8 @@
 <?php
 $items = array();
-for($i=0; $i < 10; $i++) { $items[$i] = "this is message to andi $i";
+for($i=0; $i < 10; $i++) { 
+  $items[$i]="this is message to andi $i";
 }
 header('content-Type: application/jason');
-$jsonOutput = jason_encode($items);
+$jsonOutput=jason_encode($items);
 echo $jsonOutput;
